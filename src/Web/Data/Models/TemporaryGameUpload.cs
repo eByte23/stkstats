97448789ac7684ad2b14f61806ad3c6eb57cf94a
Elijah Bate <elijahbate@gmail.com>
2023-04-07 14:28:12 +0000

    Try to add batting stats

--- src/Web/Data/Models/TemporaryGameUpload.cs
+++ src/Web/Data/Models/TemporaryGameUpload.cs
@@ -23,10 +23,30 @@ public class TemporaryPlayer
 {
     public string? DisplayName { get; set; }
     public Guid? PlayerId { get; set; }
-    public object? Batting { get; set; }
+    public TemporaryPlayerBattingStats? Batting { get; set; }
     public object? Fielding { get; set; }
     public object? Pitching { get; set; }
     public bool Found { get; set; }
+}
+
+public class TemporaryPlayerBattingStats
+{
+    public int Pa { get; set; }
+    public int Ab { get; set; }
+    public int Hits { get; set; }
+    public int Singles { get; set; }
+    public int Doubles { get; set; }
+    public int Triples { get; set; }
+    public int Hr { get; set; }
+    public int RChErr { get; set; }
+    public int Bb { get; set; }
+    public int Hbp { get; set; }
+    public int So { get; set; }
+    public int SacFly { get; set; }
+    public int Cs { get; set; }
+    public int Runs { get; set; }
+    public int Rbi { get; set; }
+    public int Sb { get; set; }
 }
 
 public class TemporaryTeam
